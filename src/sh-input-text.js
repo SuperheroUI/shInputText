646a68f104b7341aa2c873704a15bd6cc9714f45
kbatchelor <kbatchelor@insidesales.com>
2016-08-12 13:46:53 -0600

    pass through other properties

--- src/sh-input-text.js
+++ src/sh-input-text.js
@@ -4,66 +4,87 @@ require('./sh-input-text.scss');
 
 class ShInputText extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      classList: ['sh-input-text empty'],
-      placeholderText: '+'
+    constructor(props) {
+        super(props);
+        this.state = {
+            classList: ['sh-input-text empty'],
+            placeholderText: '+'
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+        this.handleFocus = this.handleFocus.bind(this);
+    }
+
+    componentDidMount() {
+        if (this.props.value) {
+            this.setState(
+                {
+                    value: this.props.value,
+                    classList: ['sh-input-text']
+                }
+            )
+        }
+
+        if (this.props.required) {
+            this.state.placeholderText = 'Required Field';
+            this.setState(this.state);
+        }
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+        this.props.onChange(event);
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
-  }
 
-  componentDidMount() {
-    if (this.props.value) {
-      this.setState(
-        {
-          value: this.props.value,
-          classList: ['sh-input-text']
+    handleFocus(event) {
+        if (this.props.onFocus) {
+            this.props.onFocus(event);
         }
-      )
+        this.refs.input.select();
     }
 
-    if(this.props.required){
-      this.state.placeholderText = 'Required Field';
-      this.setState(this.state);
-    }
-  }
-
-  handleChange(event) {    
-    this.setState({value: event.target.value});
-    this.props.onChange(event);
-  };
-  
-  handleBlur(){
-    this.setState(
-      {
-        value: this.state.value,
-        classList: ['sh-input-text']
-      }
-    );
-    
-    if(!this.state.value){
-      this.setState(
-        {
-          value: this.state.value,
-          classList: ['sh-input-text empty']
+    handleBlur() {
+        if (this.props.onBlur) {
+            this.props.onBlur(event);
         }
-      )
-    } 
-  }
+        this.setState(
+            {
+                value: this.state.value,
+                classList: ['sh-input-text']
+            }
+        );
 
-  render() {
-    return (
-      <div className={this.state.classList}>
-        <label>
-          <span className="label">{this.props.label}</span>
-          <input ref="input" type="text" placeholder={this.state.placeholderText} onChange={this.handleChange} onFocus={()=>{this.refs.input.select()}} onBlur={this.handleBlur} value={this.state.value} required={this.props.required}/>
-        </label>
-      </div>
-    )
-  }
+        if (!this.state.value) {
+            this.setState(
+                {
+                    value: this.state.value,
+                    classList: ['sh-input-text empty']
+                }
+            )
+        }
+    }
+
+    render() {
+        var {onFocus, onBlur, ...other} = this.props;
+
+        return (
+            <div className={this.state.classList}>
+                <label>
+                    <span className="label">{this.props.label}</span>
+                    <input ref="input" type="text"
+                        {...other}
+                           placeholder={this.state.placeholderText}
+                           onChange={this.handleChange}
+                           onFocus={this.handleFocus}
+                           onBlur={this.handleBlur}
+                           value={this.state.value}
+                    />
+                </label>
+            </div>
+        )
+    }
 }
 
 export default ShInputText;
